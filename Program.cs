d8ac1a2114b056f30b561b917dc539f305899a03
ajryan <AjRyan@faralloncapital.com>
2017-01-13 13:23:12 -0800

    accepts .tdl path

--- Program.cs
+++ Program.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -21,38 +23,45 @@ namespace MvcApp
     const string CLIENT_SECRET = "48c23e11279ee02ada0544c15b137693b8cde93d";
     const string REDIRECT_URI  = "http://127.0.0.1:5555/";
 
-    private static int    _completedCount;
+    private static string OauthTokenPath => Path.Combine(Path.GetTempPath(), "oauthtoken.txt");
+
+    private static string _tdlPath;
     private static bool   _complete;
     private static string _statusMessage = "Starting up...";
 
     public static void Main(string[] args)
     {
-      // TODO: more robust args specify .tdl path or explicit count
-      if (args.Length == 0 || !Int32.TryParse(args[0], out _completedCount))
-        throw new Exception("Pass completed count as argument.");
+      if (args.Length == 0 || !File.Exists(args[0]))
+        throw new Exception("First argument must be path to .TDL task list.");
 
-      WriteStatus($"Submitting completed task count {_completedCount} for {DateTime.Today:d}");
+      _tdlPath = args[0];
+      WriteStatus($"Submitting completed task count from {_tdlPath} for {DateTime.Today:d}");
 
-      // TODO: save auth token and avoid re-auth once retrieved
-      WriteStatus("Starting http listener.");
-      var host = new WebHostBuilder().UseKestrel()
-                                     .Configure(app => app.Run(ProccessRequest))
-                                     .UseUrls(REDIRECT_URI)
-                                     .Build();
+      if (File.Exists(OauthTokenPath))
+      {
+        WriteStatus($"Using existing oauth token from {OauthTokenPath}.");
+        string oauthToken = File.ReadAllText(OauthTokenPath);
+        Task.Run(async () => await SubmitCompletion(oauthToken)).Wait(); // TODO: probably OK to just kick and go since we spin on _complete later...
+      }
+      else
+      {
+        WriteStatus("Starting http listener.");
+        var host = new WebHostBuilder().UseKestrel()
+                                       .Configure(app => app.Run(ProccessRequest))
+                                       .UseUrls(REDIRECT_URI)
+                                       .Build();
 
-      new TaskFactory().StartNew(() => host.Run());
+        new TaskFactory().StartNew(() => host.Run());
 
-
-      WriteStatus("Browsing to authentication URL.");
-      var authUrl = $"https://exist.io/oauth2/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=read+write";
-      OpenBrowser(authUrl);
+        WriteStatus("Browsing to authentication URL.");
+        var authUrl = $"https://exist.io/oauth2/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=read+write";
+        OpenBrowser(authUrl);
+      }
 
       while (!_complete)
         System.Threading.Thread.Sleep(200);
 
-      System.Threading.Thread.Sleep(5000);
-      //Console.WriteLine("Press any key to exit...");
-      //Console.ReadKey();
+      System.Threading.Thread.Sleep(2000);
     }
 
     private static async Task ProccessRequest(HttpContext context)
@@ -82,35 +91,62 @@ namespace MvcApp
 
       await context.Response.WriteAsync($"<html>{refreshMeta}<head></head><body>{_statusMessage}</body></html>");
 
-      if (!isValid)
-        return;
+      if (isValid)
+      {
+        string oauthCode = context.Request.Query["code"].ToString();
+        
+        using (var client = new HttpClient())
+        {
+          var tokenResponseMessage = await client.PostAsync(
+            new Uri("https://exist.io/oauth2/access_token"),
+            new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+              { "grant_type",    "authorization_code" },
+              { "code",          oauthCode },
+              { "client_id",     CLIENT_ID },
+              { "client_secret", CLIENT_SECRET },
+              { "redirect_uri",  REDIRECT_URI }
+            }));
 
-      string oauthCode = context.Request.Query["code"].ToString();
+          var tokenResponseBody = await tokenResponseMessage.Content.ReadAsStringAsync();
+          var tokenResponse     = JsonConvert.DeserializeObject<TokenResponse>(tokenResponseBody);
+        
+          File.WriteAllText(OauthTokenPath, tokenResponse.access_token);
+          await SubmitCompletion(tokenResponse.access_token);
+        }
+      }
+    }
 
+    private static readonly Regex _doneRegex = new Regex($"DONEDATESTRING=\"({DateTime.Today.Year}-{DateTime.Today.Month:00}-{DateTime.Today.Day:00}|{DateTime.Today.Month}/{DateTime.Today.Day}/{DateTime.Today.Year})", RegexOptions.Compiled);
+
+    private static int ParseCompletedCount()
+    {
+      string tdlText     = File.ReadAllText(_tdlPath);
+      string doneTdlText = File.ReadAllText(_tdlPath.Replace(".tdl", ".done.tdl"));
+
+      int mainMatchCount    = _doneRegex.Matches(tdlText).Count;
+      int archiveMatchCount = _doneRegex.Matches(doneTdlText).Count;
+
+      WriteStatus($"Found {mainMatchCount} complete in main task list, {archiveMatchCount} in archive.");
+
+      return mainMatchCount + archiveMatchCount;
+    }
+
+    private static async Task SubmitCompletion(string oauthToken)
+    {
       using (var client = new HttpClient())
       {
-        var tokenResponseMessage = await client.PostAsync(
-          new Uri("https://exist.io/oauth2/access_token"),
-          new FormUrlEncodedContent(new Dictionary<string, string>
-          {
-            { "grant_type",    "authorization_code" },
-            { "code",          oauthCode },
-            { "client_id",     CLIENT_ID },
-            { "client_secret", CLIENT_SECRET },
-            { "redirect_uri",  REDIRECT_URI }
-          }));
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", oauthToken);
 
-        var tokenResponseBody = await tokenResponseMessage.Content.ReadAsStringAsync();
-        var tokenResponse     = JsonConvert.DeserializeObject<TokenResponse>(tokenResponseBody);
+        int completedCount = ParseCompletedCount();
+        WriteStatus($"Submitting {completedCount} completed task count.");
 
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse.access_token);
-
-        var attributeUpdateRequests        = new[] { new AttributeUpdateRequest("tasks_completed", DateTime.Today, _completedCount) };
+        var attributeUpdateRequests        = new[] { new AttributeUpdateRequest("tasks_completed", DateTime.Today, completedCount) };
         var attributeUpdateResponseMessage = await client.PostAsync(
           new Uri("https://exist.io/api/1/attributes/update/"),
           new StringContent(JsonConvert.SerializeObject(attributeUpdateRequests), Encoding.UTF8, "application/json"));
 
-        WriteStatus($"Got response {attributeUpdateResponseMessage.StatusCode:D} {attributeUpdateResponseMessage.ReasonPhrase} from updating tasks_completed.");
+        WriteStatus($"All done. Got response {attributeUpdateResponseMessage.StatusCode:D} {attributeUpdateResponseMessage.ReasonPhrase} from updating tasks_completed.");
       }
 
       _complete = true;
